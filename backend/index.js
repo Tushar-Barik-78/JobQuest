a43946c42dd09bb8ce8dc68af7c9b15360529131
Tushar Barik <tusharbarik64@gmail.com>
2026-01-22 15:04:52 +0530

    Remove trailing slash from CORS origin URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobquest-tushar.onrender.com/',
+    origin:'https://jobquest-tushar.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
